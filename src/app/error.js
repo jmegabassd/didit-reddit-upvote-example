c525f8c570ce6896151e3cb6afc7b2448d17b351
jmegabassd <jmegabassd@gmail.com>
2025-10-22 15:05:38 +0100

    Error page update

--- src/app/error.js
+++ src/app/error.js
@@ -1,12 +1,16 @@
 "use client";
 import { LoginButton } from "@/components/LoginButton";
+import Image from "next/image";
+import ohNoes from "@/../public/ohnoes.jpg";
 
 export default function Error({ error, reset }) {
   return (
     <html>
-      <body>
+      <body className="flex flex-col justify-center items-center">
+        <Image src={ohNoes} alt="Cat says oh noes!" width="400" height="359" />
+        <p>Oh noes something went wrong!</p>
         <p>{error.message}</p>
-        <LoginButton />
+        <LoginButton onClick={() => reset()} />
       </body>
     </html>
   );
