218df62e0ded4ed623869512a917389688608e59
jmegabassd <jmegabassd@gmail.com>
2025-10-22 11:43:56 +0100

    Add: Meta title to pages

--- src/app/post/[postId]/page.jsx
+++ src/app/post/[postId]/page.jsx
@@ -3,6 +3,11 @@ import { CommentList } from "@/components/CommentList";
 import { Vote } from "@/components/Vote";
 import { db } from "@/db";
 
+export const metadata = {
+  title: post.title,
+  description: "A simple blog built with Next.js",
+};
+
 export default async function SinglePostPage({ params }) {
   const postId = params.postId;
 
